9d6793852faca612abf02d82c33eb9c7eb311432
Thomps Olamide <166723801+Thomps99@users.noreply.github.com>
2024-04-29 11:20:25 +0100

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,68 @@
+#VARIABLES
+#message = "Hello World!"
+#message_2 = "Hello friends, i am learning python for cloud computing"
+#message_2 = "Hello friends, i am learning about variables in python "
+#print(message)
+#print(message_2)
+
+
+#CREATING MORE VARIABLES ON PYTHON...........................................
+
+#name = "Olamide"
+#name_2 = "Samuel"
+#address = "Ado"
+#_surname = "Ogunlade"
+
+#print(name)
+#print(name_2)
+#print(address)
+#print(_surname)
+
+message = "Hello World!"
+print(message)
+
+me2you ='hi, my name is Olamide, i am learning python'
+print(me2you)
+
+sentence = "that is my brother's car"
+print(sentence.upper())
+
+
+message = "i have begun learning about strings in python"
+print(message.upper())
+
+first_name = "COUNTRY"
+print(first_name.lower())
+
+statement = "jimmy is my life"
+print(statement.upper())
+
+#USING VARIABLES WITH STRINGS
+
+my_first_name = "Olamide"
+my_second_name = "Samuel"
+
+#fullname = f"{my_first_name} {my_second_name}"
+#print(fullname)
+
+fullname = my_first_name + " " + my_second_name
+print(fullname)
+
+nick_name = " Olamide "
+print(nick_name.strip())
+print(nick_name)
+print(nick_name.lstrip())
+print(nick_name.rstrip())
+
+
+
+#DATATYPES
+
+
+
+
+
+
+
+
+
