214493d5e42fce84560911c083f58a81b4cb9f4a
Thomps Olamide <166723801+Thomps99@users.noreply.github.com>
2024-04-29 11:19:52 +0100

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,16 @@
+# DATA TYPES EXAMPLES
+
+age = 34 # this is an integer
+price = 500.6 # this is a float
+name = "Texas" #this is a string
+is_fun = True # this is a boolean
+
+print(age, type(age))
+print(price, type(price))
+print(name, type(name))
+print(is_fun, type(is_fun))
+
+x = 3
+print(x, type(x))
+x = 3.7
+print(x, type(x))
